c4beccb070858022db76dff1bc2d837ec58ac5ce
Jeni Dang <jenipdang@yahoo.com>
2022-04-14 21:37:49 -0700

    commit tree 66426961b4a81e7fb5ced9b7a318b36b9832918a

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,17 @@ class MoviesController < ApplicationController
     render json: movie
   end
 
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer #each_serializer tells the app to use the custom movie summary serializer to render each of the movies in the collection
+  end
+
+
   private
 
   def render_not_found_response
